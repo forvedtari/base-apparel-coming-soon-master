b4418128c5d1da93232a6b15132e2c3a3edb4d8e
“Vedanti <vedantitari133@gmail.com>
2023-01-24 10:36:43 +0530

    final

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
  const submit= document.getElementById('submit');
  const email = document.getElementById('email_input');
- let error_message1=document.getElementById("error_message1");
-const error_message2=document.getElementById("error_message2");
- let error=document.getElementById("error")
+const  error_message1=document.getElementById("error_message1");
+const  error_message2=document.getElementById("error_message2");
+const success = document.getElementById("success_message")
+const  error=document.getElementById("error")
  submit.onclick=function() {
+    console.log(email.value)
     var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
-    if(email.value== " ")
-    {
-        error_message1.style.display="none";
-        error_message2.style.display="inline";
-        error.style.display="block"
-        return true;
-    }
-    else if(email.value.match(mailformat)){
-        alert(" You've entered a valid Email")
+    if(email.value.match(/^([a-z0-9]|([a-z0-9])){2,30}[@]([a-z0-9]){2,15}[.][a-z0-9]{2,17}$/)){
+        
         error_message1.style.display="none";
         error_message2.style.display="none";
         error.style.display="none"
-
+        success.style.display='block'
+        success.innerHTML=" You have successfully signed up for adventure with us!"
+        
+    }
+    else if(email.value.length==0)
+    {   success.style.display="none";
+        error_message1.style.display="none";
+        error_message2.style.display="inline";
+        error.style.display="block"
+        
     }
     else{
+        success.style.display="none";
         error_message2.style.display="none"
         error_message1.style.display="inline";
         error.style.display="block"
-        return false;
+        
     }
     
     
-    console.log(email.value)};
+};
